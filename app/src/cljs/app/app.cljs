6efa3238701db72f2446a93475d5ba36bc5c2a85
mswift42 <martin.haesler@gmail.com>
2016-01-12 18:14:17 +0000

    add fixed-header-and-drawer-component

--- app/src/cljs/app/app.cljs
+++ app/src/cljs/app/app.cljs
@@ -1,6 +1,28 @@
-(ns app.app)
+(ns app.app
+  (:require [cljsjs.material]
+            [reagent.core :as reagent]))
+
+(defn fixed-header-and-drawer-component [content]
+  [:div.mdl-layout.mdl-js-layout.mdl-layout--fixed-drawer.mdl-layout--fixed-header
+   [:header.mdl-layout__header
+    [:div.mdl-layout__header-row
+     [:div.mdl-layout-spacer]]]
+   [:div.mdl-layout__drawer
+    [:span.mdl-layout-title "Resume"]
+    [:nav.mdl-navigation
+     [:a.mdl-navigation__link {:href "#"} "About"]]]
+   [:main.mdl-layout__content
+    [:div.page-content
+     content]]])
+
+(defn some-component []
+  [:div
+   [fixed-header-and-drawer-component
+    [:h3 "This is your app"]]])
+   
+
+
 
 (defn init []
-  (let [c (.. js/document (createElement "DIV"))]
-    (aset c "innerHTML" "<p>i'm dynamically created</p>")
-    (.. js/document (getElementById "container") (appendChild c))))
+  (reagent/render-component [some-component]
+                            (.getElementById js/document "container")))
