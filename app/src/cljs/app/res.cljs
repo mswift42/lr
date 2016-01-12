2208475cfc055fd0bba5a719af519359079b998c
mswift42 <martin.haesler@gmail.com>
2016-01-12 20:01:43 +0000

    add res file

---
+++ app/src/cljs/app/res.cljs
@@ -0,0 +1,4 @@
+(ns app.res)
+
+(def resume
+  {:profile "Martin Haesler is a former sales man for plumbing and heating projects, who after 17 years of working in this line of work, decided it was time for a change, see something of the world, and try for a completely different career path. Having always been interested in computer technology, an avid Linux user for many years, he decided on programming as a new career.\nMartin Haesler is quick to learn new things, self motivated and eager to improve."})
