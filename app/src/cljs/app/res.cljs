1fec5ca54513015ecb3446af1020a22c6922007e
mswift42 <martin.haesler@gmail.com>
2016-01-12 20:10:58 +0000

    add contact and projects section

--- app/src/cljs/app/res.cljs
+++ app/src/cljs/app/res.cljs
@@ -1,4 +1,10 @@
 (ns app.res)
 
 (def resume
-  {:profile "Martin Haesler is a former sales man for plumbing and heating projects, who after 17 years of working in this line of work, decided it was time for a change, see something of the world, and try for a completely different career path. Having always been interested in computer technology, an avid Linux user for many years, he decided on programming as a new career.\nMartin Haesler is quick to learn new things, self motivated and eager to improve."})
+  {:profile "Martin Haesler is a former sales man for plumbing and heating projects, who after 17 years of working in this line of work, decided it was time for a change, see something of the world, and try for a completely different career path. Having always been interested in computer technology, an avid Linux user for many years, he decided on programming as a new career.\nMartin Haesler is quick to learn new things, self motivated and eager to improve."
+   :contact {:name "Martin Haesler"
+             :email "martin.haesler@gmail.com"
+             :phone "07481 598020"}
+   :projects {:title "ThemeCreator"
+              :url "https://github.com/mswift42/themecreator"
+              :description "ThemeCreator lets you easily create beautiful color themes for IntelliJ Editors, Atom, Textmate and Emacs.\nThemeCreator is a Clojurescript app, built using the reagent framework."}})
