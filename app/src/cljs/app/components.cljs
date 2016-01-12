dccf15f491d408193b284984d2cce45e6408171d
mswift42 <martin.haesler@gmail.com>
2016-01-12 20:30:11 +0000

    add contact-component

--- app/src/cljs/app/components.cljs
+++ app/src/cljs/app/components.cljs
@@ -14,3 +14,21 @@
       [:h3 "Profile"]]
      [:div.aboutdescription
       description]]]])
+
+(defn contact-component
+  [name email phone]
+  [:div#contact
+   [section-component
+    [:div.contact
+     [:div.contactname
+      [:h3 name]]
+     [:div.contactphonemail.mdl-grid
+      [:div.mdl-cell.mdl-cell--4-col
+       [:i.material-icons "phone"]]
+      [:div.mdl-cell.mdl-cell--4-col
+       [:i.material-icons "email"]]]
+     [:div.contact.mdl-grid
+      [:div.mdl-cell.mdl-cell--4-col
+       [:p phone]]
+      [:div.mdl-cell.mdl-cell--4-col
+       [:p email]]]]]])
