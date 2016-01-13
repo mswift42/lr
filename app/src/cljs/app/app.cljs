5c035f9069adef20d16a663b4cb672cfb8b9a639
mswift42 <martin.haesler@gmail.com>
2016-01-13 17:07:50 +0000

    Add projects section to sidebar

--- app/src/cljs/app/app.cljs
+++ app/src/cljs/app/app.cljs
@@ -1,6 +1,8 @@
 (ns app.app
   (:require [cljsjs.material]
-            [reagent.core :as reagent]))
+            [reagent.core :as reagent]
+            [app.components :as comps]
+            [app.res :refer [resume]]))
 
 (defn fixed-header-and-drawer-component [content]
   [:div.mdl-layout.mdl-js-layout.mdl-layout--fixed-drawer.mdl-layout--fixed-header
@@ -10,19 +12,28 @@
    [:div.mdl-layout__drawer
     [:span.mdl-layout-title "Resume"]
     [:nav.mdl-navigation
-     [:a.mdl-navigation__link {:href "#"} "About"]]]
+     [:a.mdl-navigation__link {:href "#contact"} "Contact"]
+     [:a.mdl-navigation__link {:href "#about"} "About"]
+     [:a.mdl-navigation__link {:href "#projects"} "Projects"]]]
    [:main.mdl-layout__content
     [:div.page-content
      content]]])
 
-(defn some-component []
+(defn main-component []
   [:div
    [fixed-header-and-drawer-component
-    [:h3 "This is your app"]]])
-   
+    [:div
+     [comps/contact-component (:name (:contact resume))
+      (:email (:contact resume))
+      (:phone (:contact resume))]
+     [comps/about-component
+      [:p (:profile resume)]]
+     [comps/projects-component
+      (:projects resume)]]]])
+
 
 
 
 (defn init []
-  (reagent/render-component [some-component]
+  (reagent/render-component [main-component]
                             (.getElementById js/document "container")))
